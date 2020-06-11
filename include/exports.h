d4bb38ef5a1c35f44bc10531181923485bc8ed37
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2020-06-11 22:13:57 +0200

    Apply PATCH1/55-include_exports.h

--- include/exports.h
+++ include/exports.h
@@ -5,6 +5,10 @@
 
 #include <common.h>
 
+#ifdef CONFIG_EXPORT_ETH_FNS
+typedef void	rxhand_func(uchar *, unsigned, unsigned, unsigned);
+#endif /* EXPORT_ETH_FNS */
+
 /* These are declarations of exported functions available in C code */
 unsigned long get_version(void);
 int  getc(void);
@@ -24,6 +28,13 @@ void do_reset (void);
 int i2c_write (uchar, uint, int , uchar* , int);
 int i2c_read (uchar, uint, int , uchar* , int);
 #endif	/* CFG_CMD_I2C */
+#ifdef CONFIG_EXPORT_ETH_FNS
+void NetSetHandler(rxhand_func*);	
+int eth_init(bd_t *bis);			
+int eth_send(volatile void *packet, int length);	   
+int eth_rx(void);			
+void eth_halt(void);
+#endif /* EXPORT_ETH_FNS */
 
 void app_startup(char **);
 
