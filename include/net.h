6126cd90765bfc5a33edce3690c1c4300d6d1559
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2020-06-11 22:13:05 +0200

    Apply PATCH1/04-r2c-net.h.patch

--- include/net.h
+++ include/net.h
@@ -336,7 +336,9 @@ extern int		NetState;		/* Network loop state		*/
 extern int		NetRestartWrap;		/* Tried all network devices	*/
 #endif
 
-typedef enum { BOOTP, RARP, ARP, TFTP, DHCP, PING, DNS, NFS, CDP, NETCONS, SNTP } proto_t;
+//typedef enum { BOOTP, RARP, ARP, TFTP, DHCP, PING, DNS, NFS, CDP, NETCONS, SNTP } proto_t;
+//AMI Extension - R2C Protocol
+typedef enum { BOOTP, RARP, ARP, TFTP, DHCP, PING, DNS, NFS, CDP, NETCONS, SNTP, R2C } proto_t;
 
 /* from net/net.c */
 extern char	BootFile[128];			/* Boot File name		*/
