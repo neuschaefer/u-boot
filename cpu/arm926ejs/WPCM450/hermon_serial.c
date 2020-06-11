4a7559507349d91e886bb944eb0ae001b7dc7831
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2020-06-11 22:15:25 +0200

    Apply PATCH5/31-cpu_arm926ejs_WPCM450_hermon_serial.c

--- cpu/arm926ejs/WPCM450/hermon_serial.c
+++ cpu/arm926ejs/WPCM450/hermon_serial.c
@@ -21,11 +21,14 @@ WPCM_global_uart_init(void)
   /* Select 48M clock source for UART in case that real chip */
   SET_FIELD(*(volatile unsigned long *)CLK_SEL, CLK_UART_SRC, CLK_48M_SRC_SEL);
   
+  /* Serial Interface pins 1 selected. For X8DTT-IBX & X8DTL only */
+  SET_BIT(*(volatile unsigned long *)GCR_MFSEL1, MF_HSP1SEL_BIT);
+
   /* Serial Interface pins 2 selected. */
   SET_BIT(*(volatile unsigned long *)GCR_MFSEL1, MF_HSP2SEL_BIT);
 
   /* Serial Port Mode 2 selected. (Core Take-ove) */
-  SET_FIELD( *(volatile unsigned long *)GCR_SPSWC, SPSWC_SPMOD, SPSWC_SPMOD_3);	
+  SET_FIELD( *(volatile unsigned long *)GCR_SPSWC, SPSWC_SPMOD, SPSWC_SPMOD_3);	 
 
   return;
 }
