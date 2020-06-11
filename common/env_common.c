f3350e6c14964492b366f44b6cd2ae93d967fda8
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2020-06-11 22:14:48 +0200

    Apply PATCH3/06-common_env_common.c

--- common/env_common.c
+++ common/env_common.c
@@ -99,6 +99,9 @@ uchar default_environment[] = {
 #ifdef	CONFIG_ETH3ADDR
 	"eth3addr="	MK_STR(CONFIG_ETH3ADDR)		"\0"
 #endif
+#ifdef	CONFIG_ETHPRIME
+	"ethprime="	CONFIG_ETHPRIME			"\0"
+#endif
 #ifdef	CONFIG_IPADDR
 	"ipaddr="	MK_STR(CONFIG_IPADDR)		"\0"
 #endif
