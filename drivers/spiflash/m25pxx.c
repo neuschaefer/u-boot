6d21e9de5017b9684f2868c3c17373c84a8daa49
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2020-06-11 22:14:24 +0200

    Apply PATCH1/81-drivers_spiflash_m25pxx.c

--- drivers/spiflash/m25pxx.c
+++ drivers/spiflash/m25pxx.c
@@ -50,6 +50,8 @@ static struct spi_flash_info m25pxx_data [] =
 	{ "ST Micro m45p32"  , 0x20, 0x1640, 0x400000 , 1, {{ 0, 64  * 1024, 64  },} },
 	{ "ST Micro m45p64"  , 0x20, 0x1740, 0x800000 , 1, {{ 0, 64  * 1024, 128 },} },
 
+	/* [SMC] added for Macronix 128 MBIT flash */
+	{ "Macrox" , 0xC2, 0x1820, 0x1000000 , 1, {{ 0, 64  * 1024, 256 },} }
 };
 
 
