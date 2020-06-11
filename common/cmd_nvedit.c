e3114f257217eea492a15c247fef8d9dfb9d6237
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2020-06-11 22:14:16 +0200

    Apply PATCH1/73-common_cmd_nvedit.c

--- common/cmd_nvedit.c
+++ common/cmd_nvedit.c
@@ -240,19 +240,22 @@ int _do_setenv (int flag, int argc, char *argv[])
 					baudrate);
 				return 1;
 			}
-			printf ("## Switch baudrate to %d bps and press ENTER ...\n",
-				baudrate);
-			udelay(50000);
-			gd->baudrate = baudrate;
-#ifdef CONFIG_PPC
-			gd->bd->bi_baudrate = baudrate;
-#endif
+			if (gd->baudrate != baudrate)
+			{
+				printf ("## Switch baudrate to %d bps and press ENTER ...\n",
+					baudrate);
+				udelay(50000);
+				gd->baudrate = baudrate;
+	#ifdef CONFIG_PPC
+				gd->bd->bi_baudrate = baudrate;
+	#endif
 
-			serial_setbrg ();
-			udelay(50000);
-			for (;;) {
-				if (getc() == '\r')
-				      break;
+				serial_setbrg ();
+				udelay(50000);
+				for (;;) {
+					if (getc() == '\r')
+					      break;
+				}
 			}
 		}
 
