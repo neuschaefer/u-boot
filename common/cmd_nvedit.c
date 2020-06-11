9e6e870c3b525210e587b0e6d3a82f5f5c0b87b4
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2020-06-11 22:14:17 +0200

    Apply PATCH1/74-common_cmd_nvedit.c

--- common/cmd_nvedit.c
+++ common/cmd_nvedit.c
@@ -404,6 +404,43 @@ int do_setenv ( cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 }
 
 /************************************************************************
+*Gets all the environment variable names
+*/
+int Getenvvar(unsigned char *count,unsigned char *VarName,int *BootVarlen) 
+{
+     int i=0,Counter=0;
+     unsigned char *Nextenv;
+     uchar *env,*nxt = NULL;
+
+     uchar *env_data = env_get_addr(0);
+     Nextenv = VarName;
+     for (env=env_data; *env; env=nxt+1)
+     {
+		i=0;
+		for (nxt=env; *nxt; ++nxt)
+		{			
+			if(*nxt != '\0')
+			{
+			      if(i==0)
+			      {
+			          *count += 1;
+				   while((*Nextenv++ = *nxt++) != '=' ) 
+				   {
+			    		Counter ++;
+				   };
+				   Nextenv --;
+		   		   *Nextenv++ = '\0';
+	    	   		   i =1;
+			      }
+			}
+              }
+      }
+     *BootVarlen = Counter + *count;
+     return 0;
+}
+
+
+/************************************************************************
  * Prompt for environment variable
  */
 
